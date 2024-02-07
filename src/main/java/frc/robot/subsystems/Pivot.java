40b59b760ce638127f96c92a6c9d7ec30377439d
AL <roboriotDev4@gmail.com>
2024-02-06 17:23:27 -0800

    Semi done pivot code w/ no limits - ARS

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -1,7 +1,13 @@
 package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.Setup;
 
 public class Pivot extends Subsystem {
-
+   public CANSparkMax pivot;
+   public double speed= 0.25;
+   public double rightJoystickPos;
    //get instance
    static Pivot mInstance = new Pivot();
 
@@ -9,14 +15,35 @@ public class Pivot extends Subsystem {
       return mInstance;
    }
 
-   public Pivot(){}
+   public Pivot(){
+      pivot = new CANSparkMax(Setup.PivotMotorID, MotorType.kBrushed);
+
+   }
+
+   public boolean AbletoDown(){
+      return rightJoystickPos >= 0.3; //&& ADD LIMITS SOMEHOW
+   }
+   public boolean AbletoUp(){
+      return rightJoystickPos<=-0.3; //&& ADD LIMITS SOMEHOW
+   }
 
    @Override
-   public void updateSubsystem(){}
+   public void updateSubsystem(){
+      rightJoystickPos = Setup.getInstance().getSecondaryDriverRightYAxis();
+      if (AbletoDown()){
+         pivot.set(-speed);
+      }else if (AbletoUp()){
+         pivot.set(speed);
+      }else{
+         pivot.set(0);
+      }
+   }
 
    @Override
    public void outputToSmartDashboard(){}
 
    @Override
-   public void stop(){}
+   public void stop(){
+      pivot.set(0);
+   }
 }
