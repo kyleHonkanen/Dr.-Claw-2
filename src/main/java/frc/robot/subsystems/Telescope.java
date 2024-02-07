0dfcd2109f08c6802bad53691a17625e0d81ef0f
AL <roboriotDev4@gmail.com>
2024-02-06 16:45:45 -0800

    Updated Telescope to move according to y of left joystick on secondary driver, attempted to add limits-ARS

--- src/main/java/frc/robot/subsystems/Telescope.java
+++ src/main/java/frc/robot/subsystems/Telescope.java
@@ -1,6 +1,17 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+import frc.robot.Setup;
+
 public class Telescope extends Subsystem {
+    CANSparkMax extendy;
+    public DigitalInput min;
+    public DigitalInput max;
+    public double speed = 0.1;
+    public double leftJoystickPos;
     
     //get instance
     static Telescope mInstance = new Telescope();
@@ -10,16 +21,38 @@ public class Telescope extends Subsystem {
     }
 
 
-    public Telescope() {}
-
+    public Telescope() {
+        extendy = new CANSparkMax(Setup.TelescopeMotorID, MotorType.kBrushed);
+        min = new DigitalInput(Setup.ArmMinID);
+        max = new DigitalInput(Setup.ArmMaxID);
+    }
+    public boolean AbletoExtend(){
+        return leftJoystickPos >= 0.3 && min.get();
+    }
+    public boolean AbletoRetract(){
+        return leftJoystickPos <= -0.3 && max.get();
+    }
   
 
     @Override
-    public void updateSubsystem(){}
+    public void updateSubsystem(){
+        leftJoystickPos = Setup.getInstance().getSecondaryDriverLeftYAxis();
+
+
+        if(AbletoExtend()){
+            extendy.set(speed);
+        }else if (AbletoRetract()){
+            extendy.set(-speed);
+        }else{
+            extendy.set(0);
+        }
+    }
 
     @Override
     public void outputToSmartDashboard(){}
 
     @Override
-    public void stop(){}
+    public void stop(){
+        extendy.set(0);
+    }
 }
